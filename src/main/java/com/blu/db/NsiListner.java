51a8dd357d1394759ebf554fb50e2a38566e5244
shamim <shamimapache@gmail.com>
2015-07-22 17:15:53 +0300

    update listner

--- src/main/java/com/blu/db/NsiListner.java
+++ src/main/java/com/blu/db/NsiListner.java
@@ -4,18 +4,48 @@ import oracle.jdbc.dcn.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by shamim on 21/07/15.
  */
 public class NsiListner implements DatabaseChangeListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(NsiListner.class);
+    private HashMap<Integer, List<ORNEvent>> events = new HashMap<Integer, List<ORNEvent>>();
 
     @Override
+    //TODO ResultSetMetaDataOptions property is set to 1
+    //todo select object_name from user_objects where object_id=73786
     public void onDatabaseChangeNotification(DatabaseChangeEvent databaseChangeEvent) {
-        QueryChangeDescription qcd =  databaseChangeEvent.getQueryChangeDescription()[0];
-        TableChangeDescription tcd = qcd.getTableChangeDescription()[0];
-        for(RowChangeDescription rcd : tcd.getRowChangeDescription()){
-            LOGGER.info("Updated Row ID:" + rcd.getRowid().stringValue() + " Operation:" + rcd.getRowOperation().name());
+
+
+
+        for(QueryChangeDescription qcd : databaseChangeEvent.getQueryChangeDescription()){
+            LOGGER.info("Query Id: {}", qcd.getQueryId());
+            LOGGER.info("Event Type: {}", qcd.getQueryChangeEventType().name());
+            Integer tableId = null;
+            final List<ORNEvent> ornEvents = new ArrayList<ORNEvent>();
+            for(TableChangeDescription tcd : qcd.getTableChangeDescription()){
+                //ClassDescriptor descriptor = OracleChangeNotificationListener.this.descriptorsByTable.get(new DatabaseTable(tcd.getTableName()));
+                LOGGER.info("table Name: {}", tcd.getTableName()); // table name is empty
+                LOGGER.info("Object ID: {}", tcd.getObjectNumber()); // use object id]]
+                tableId = tcd.getObjectNumber();
+                for(RowChangeDescription rcd : tcd.getRowChangeDescription()){
+                    LOGGER.info("Row ID:" + rcd.getRowid().stringValue() + " Operation:" + rcd.getRowOperation().name());
+                    ornEvents.add(new ORNEvent(rcd.getRowOperation().name(), rcd.getRowid().stringValue()));
+                }
+            }
+            if(tableId!=null){
+                events.put(tableId, ornEvents);
+            }
+
         }
+
+    }
+
+    public HashMap<Integer, List<ORNEvent>> getEvents() {
+        return events;
     }
 }
